bb154b524e6af5eb132ca6f034e20286e8449ecb
Jonathan Henrique <silva.henriquejonas@gmail.com>
2024-08-13 09:41:44 -0300

    Criando Projeto

---
+++ src/main/java/com/jonathan/colegiogeneration/ColegiogenerationApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.colegiogeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColegiogenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColegiogenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathan/colegiogeneration/ColegiogenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.colegiogeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColegiogenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
