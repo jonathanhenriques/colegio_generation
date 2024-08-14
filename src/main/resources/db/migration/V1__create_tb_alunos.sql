b3cf2ca9a5928068ad50b8bd0fb2f9e209c98bc2
Jonathan Henrique <silva.henriquejonas@gmail.com>
2024-08-14 18:15:45 -0300

    deploy render editando migrate=true

--- src/main/resources/db/migration/V1__create_tb_alunos.sql
+++ src/main/resources/db/migration/V1__create_tb_alunos.sql
@@ -10,3 +10,11 @@ CREATE TABLE TB_ALUNO(
 );
 
 
+
+
+
+
+INSERT INTO TB_ALUNO(nome, idade ,nota_primeiro_semestre,
+ nota_segundo_semestre ,nome_professor, numero_da_sala) VALUES (
+'Jonathan Henrique Da Silva', 27, 9.5, 9.9, 'Professor Antonio', 71
+);
